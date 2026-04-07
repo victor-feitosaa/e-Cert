120e4ff42d30bd646444400074722bad7c4deb5c
victor-feitosaa <dev.victorfeitosa@outlook.com>
2026-04-07 13:09:26 -0500

    ADD: register scheema

--- apps/shared/src/schemas/auth/auth.ts
+++ apps/shared/src/schemas/auth/auth.ts
@@ -6,3 +6,12 @@ export const loginSchema = z.object({
 });
 
 export type LoginInput = z.infer<typeof loginSchema>;
+
+export const registerSchema = z.object({
+  name: z.string().min(2, "Mínimo 2 caracteres"),
+  email: z.string().email("E-mail inválido"),
+  password: z.string().min(4, "Mínimo 4 caracteres"),
+  confirmPassword: z.string().min(4, "Mínimo 4 caracteres")
+});
+
+export type RegisterInput = z.infer<typeof registerSchema>;
