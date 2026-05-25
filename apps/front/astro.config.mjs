bd19845ccd9d28ed246b8b73321ef323823511e6
victor-feitosaa <dev.victorfeitosa@outlook.com>
2026-05-24 22:00:15 -0500

    fix config

--- apps/front/astro.config.mjs
+++ apps/front/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   integrations: [react()],
   vite: {
