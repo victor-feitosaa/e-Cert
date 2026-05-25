5b447acffc3ee704f57b3207cd8753f9879d01ba
victor-feitosaa <dev.victorfeitosa@outlook.com>
2026-05-24 20:50:50 -0500

    teste deploy

--- apps/front/astro.config.mjs
+++ apps/front/astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()
-],
-  
-
+  output: 'server',
+  adapter: vercel(),
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
   }
