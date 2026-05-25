cbaa67724e3031fd9232f5757fcf15d9b5b4691b
victor-feitosaa <dev.victorfeitosa@outlook.com>
2026-05-24 20:52:41 -0500

    fix import

--- apps/front/astro.config.mjs
+++ apps/front/astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
